4eb822f47b2e173eddb4464ef7d97b45777e8d6e
fg2310 <62403481+fg2310@users.noreply.github.com>
2025-07-22 03:23:26 +0200

    Delete Chefs.Api directory

--- Chefs.Api/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using System.Collections.Immutable;
-global using System.Text.Json;
-global using System.Text.Json.Serialization;
-global using Chefs.Api.Entities;
-global using Chefs.Api.Serialization;
-global using Microsoft.AspNetCore.Mvc;
